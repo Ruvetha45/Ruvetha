edc3bcbfa08329b5ad4f64599e74858b89d693fd
Ruvetha45 <sruvetha8@gmail.com>
2024-11-08 16:26:52 +0530

    resume url update

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -11,7 +11,7 @@ const Home = ({id}) => {
   const darkMode = theme.state.darkMode;
 
   const handleResumeClick = () => {
-    const resumeUrl = 'https://drive.google.com/file/d/1C277fvxyjJvWrRIvqrdSS-eU3f0luUQR/view?usp=drivesdk';
+    const resumeUrl = 'https://drive.google.com/file/d/1CnvfhOkQY84z4StAaSuF4Qp8W_XiM00r/view?usp=drivesdk';
     window.open(resumeUrl, '_blank'); 
   };
 
